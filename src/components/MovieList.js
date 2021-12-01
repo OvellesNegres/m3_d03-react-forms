6e0f0cac20f76efce692d6b109a76e582ecb1adb
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-12-01 14:47:56 +0100

    done

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,13 +2,39 @@ import { useState } from "react";
 import moviesDataJSON from "../movies-data.json";
 import MovieCard from "./MovieCard";
 
-// import AddMovie from "./AddMovie";
-// import FilterMovies from "./FilterMovies";
+import AddMovie from "./AddMovie";
+import FilterMovies from "./FilterMovies";
 
 function MovieList() {
+
+  const [movies, setMovies] = useState(moviesDataJSON)
+  const [filteredMovies, setFilteredMovies] = useState(moviesDataJSON)
+
+  // const [state, setState] = useState({
+  //  movies: moviesDataJSON,
+  //  filteredMovies: moviesDataJSON
+  // })
+  
+  function createMovie(body){
+    body._id = parseInt(Math.random() * 10000)
+    setMovies([body, ...movies])  // movies.concat([body])
+
+  }
+
+  function handleFilterMovies(letter){
+    if(letter === "") {setFilteredMovies(movies)}
+    else{
+      setFilteredMovies(
+        movies.filter(movie=>movie.title[0].toLowerCase() === letter.toLowerCase())
+      )}
+  }
+
   return (
     <div>
-      {moviesDataJSON.map((movie) => {
+      <FilterMovies filterMovies={handleFilterMovies} />
+      <AddMovie parentCb={createMovie} />
+
+      {filteredMovies.map((movie) => {
         return <MovieCard key={movie._id} movie={movie} />;
       })}
     </div>
